cc212c0cdfe13f64088a936186521308f9d39f0c
LeeSeonHee <leeseonhee17@gmail.com>
2023-08-08 01:56:55 +0900

    nuxt-link사용

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
-<template>
+`<template>
     <div>
+        <header>
+        <!-- nuxt-link 사용하기 -->
         <h1>{{ $route.name }} 페이지</h1>
+        <NuxtLink to="/">홈페이지</NuxtLink>
+        <NuxtLink to="/main">메인 페이지</NuxtLink>
+        <NuxtLink to="/product">상품페이지</NuxtLink>
+        <!-- <router-link></router-link> -->
+        </header>
         <Nuxt />
     </div>
 </template>

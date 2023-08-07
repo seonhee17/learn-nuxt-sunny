dcdd143deffd74bca78dcc4131ec012bd90097ee
LeeSeonHee <leeseonhee17@gmail.com>
2023-08-07 23:02:42 +0900

    페이지 컴포넌트 빌드 결과 확인

---
+++ pages/product.vue
@@ -0,0 +1,19 @@
+<!--
+    /pages/product/index.vue와 
+    /pages/product.vue 는 동일한 라우터 경로를 잡는것을 
+    /.nuxt/router.js routes 변수에서 확인할 수 있다.
+
+-->
+<template>
+    <div>
+        <h2>productPage</h2>
+    </div>
+
+</template>
+  
+  <script>
+  export default {
+    name: 'ProductIndexPage',
+  }
+  </script>
+  
